b68cb1308f240a085bd9ea41be68a87f5ffed735
Pedro Aguiar <paguiar32@gmail.com>
2020-02-06 22:22:28 +0100

    [add] Strip lines, keep only french section, different indenting

--- wiktionary_vocabulary.rb
+++ wiktionary_vocabulary.rb
@@ -1,27 +1,37 @@
 require_relative 'wiktionary_page'
 
 class WiktionaryVocabulary < WiktionaryPage
-    def handle_page
-        super
+  def handle_page
+    super
 
-        if @title_is_of_interest
-            #puts @page_text
+    if @title_is_of_interest
+      #puts @page_text
 
-            if page_text_fr = french_only
+      if page_text_fr = french_only
 
-              cached_name = "cache/#{@page_title}"
-              IO.write(cached_name, page_text_fr)
-            end
+        cached_name = "cache/#{@page_title}"
+        IO.write(cached_name, page_text_fr)
+      end
 
-        end
+    end
+  end
+
+  def french_only
+    text_split = @page_text.split("\n").map do |line|
+      line.strip
+    end.join("\n")
+
+    if m = @page_text.match(/^==\s?{{langue\|fr}}\s?==[^=](.+)/m)
+      before_french_removed = m[1]
+      if m = before_french_removed.match(/(.+?)^==[^=]/m)
+        french = m[1]
+      else
+        french = before_french_removed
+      end
+    else
+      #puts "French not found!?!"
+      return nil
     end
 
-    def french_only
-        if m = @page_text.match(/^== {{langue\|fr}} ==.*?^==[^=]/m)
-            m[0]
-        else
-            #puts "French not found!?!"
-            nil
-        end
-    end
+  end
 end
